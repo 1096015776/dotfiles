b10469f4f93a251dbb0c26a2c12d9c700bee051b
fangxi <1096015776@qq.com>
2025-01-16 20:17:58 +0800

    ignore ai build

--- dots/.config/nvim/lua/plugins/lsp.lua
+++ dots/.config/nvim/lua/plugins/lsp.lua
@@ -55,7 +55,7 @@ return {
     "Exafunction/codeium.nvim",
     cmd = "Codeium",
     event = "InsertEnter",
-    build = ":Codeium Auth",
+    -- build = ":Codeium Auth",
     opts = {
       enable_cmp_source = vim.g.ai_cmp,
       virtual_text = {
