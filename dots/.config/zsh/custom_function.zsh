8d2a528fcd1718a53bacf63608145ed4a258c8a6
fangxi <1096015776@qq.com>
2025-02-15 23:04:04 +0800

    add nvm

--- dots/.config/zsh/custom_function.zsh
+++ dots/.config/zsh/custom_function.zsh
@@ -6,3 +6,8 @@ function proxy_off(){
   unset no_proxy
   unset http_proxy https_proxy ALL_PROXY
 }
+
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
