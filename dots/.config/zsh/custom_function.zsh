47fd08c2a2b6ee2c057eaeed9c832ae56b2aa5a9
fangxi <1096015776@qq.com>
2025-02-16 13:42:23 +0800

    remove nvm

--- dots/.config/zsh/custom_function.zsh
+++ dots/.config/zsh/custom_function.zsh
@@ -7,7 +7,3 @@ function proxy_off(){
   unset http_proxy https_proxy ALL_PROXY
 }
 
-export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
