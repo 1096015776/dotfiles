2e89bc7fbb9c324b84271ef81eb90c679a6dfafd
fangxi <1096015776@qq.com>
2025-03-03 21:56:46 +0800

    mod shutkey

--- dots/.config/nvim/ftplugin/javascript.lua
+++ dots/.config/nvim/ftplugin/javascript.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("i", "t", require("fx.javascript").add_async, { buffer = true })
-vim.keymap.set("n", "<leader>r", "i{({ <++> }) => <++>.<++>}<esc>%")
+vim.keymap.set("n", "<leader>r", "a${({ <++> }) => <++>.<++>}<esc>%")
 vim.keymap.set("i", "<c-l>", "<esc>/<++>/<cr>c4l")
