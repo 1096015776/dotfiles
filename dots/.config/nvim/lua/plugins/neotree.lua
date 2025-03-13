ace91cc8078b74c2e78fe8b75e3db39d4859e37a
fangxi <1096015776@qq.com>
2025-03-13 12:07:36 +0800

    fix:将neo-tree分屏大小改小

--- dots/.config/nvim/lua/plugins/neotree.lua
+++ dots/.config/nvim/lua/plugins/neotree.lua
@@ -2,6 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {
+      width = 25,
       mappings = {
         ["<c-f>"] = function(state)
           local node = state.tree:get_node()
